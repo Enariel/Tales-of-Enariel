e2e097ae7b3c14df5e600264eca4a68115d21b3d
Enariel <56527754+Enariel@users.noreply.github.com>
2021-01-02 16:30:47 -0500

    Let the Games Begin!
    Main Asset folder push to main.

---
+++ Scripts/Input/Controls.cs
@@ -0,0 +1,234 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Movement"",
+            ""id"": ""170e1884-73a9-4f6a-991d-d1e069e9147a"",
+            ""actions"": [
+                {
+                    ""name"": ""Walk"",
+                    ""type"": ""Value"",
+                    ""id"": ""04adef75-37ea-43b1-9711-640f7b9caa83"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""f4d00d4f-12d0-4e6f-be47-7bc2837aff99"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""f2b10fc0-ac38-4b85-a0d9-321f3f401934"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Walk_WASD"",
+                    ""id"": ""b42d9bdf-1aad-44c3-a508-dabf44ca5df1"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""3a711673-ee25-4420-a9ee-e009faef1050"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""49714304-94ff-4ebb-9fca-d35c1c30d8cb"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""b71d7fdd-4f55-4320-85d9-5e8d7ceed182"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""faf75b6b-c4ec-4d1d-ad8e-bc46173ab912"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""53543124-b4c0-4631-a96e-1149da25daea"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bb4fe63e-0fd3-4af8-9bb0-eedf22758df4"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Movement
+        m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
+        m_Movement_Walk = m_Movement.FindAction("Walk", throwIfNotFound: true);
+        m_Movement_Run = m_Movement.FindAction("Run", throwIfNotFound: true);
+        m_Movement_Interact = m_Movement.FindAction("Interact", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Movement
+    private readonly InputActionMap m_Movement;
+    private IMovementActions m_MovementActionsCallbackInterface;
+    private readonly InputAction m_Movement_Walk;
+    private readonly InputAction m_Movement_Run;
+    private readonly InputAction m_Movement_Interact;
+    public struct MovementActions
+    {
+        private @Controls m_Wrapper;
+        public MovementActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Walk => m_Wrapper.m_Movement_Walk;
+        public InputAction @Run => m_Wrapper.m_Movement_Run;
+        public InputAction @Interact => m_Wrapper.m_Movement_Interact;
+        public InputActionMap Get() { return m_Wrapper.m_Movement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MovementActions set) { return set.Get(); }
+        public void SetCallbacks(IMovementActions instance)
+        {
+            if (m_Wrapper.m_MovementActionsCallbackInterface != null)
+            {
+                @Walk.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnWalk;
+                @Walk.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnWalk;
+                @Walk.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnWalk;
+                @Run.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnRun;
+                @Interact.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnInteract;
+                @Interact.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnInteract;
+                @Interact.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnInteract;
+            }
+            m_Wrapper.m_MovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Walk.started += instance.OnWalk;
+                @Walk.performed += instance.OnWalk;
+                @Walk.canceled += instance.OnWalk;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+                @Interact.started += instance.OnInteract;
+                @Interact.performed += instance.OnInteract;
+                @Interact.canceled += instance.OnInteract;
+            }
+        }
+    }
+    public MovementActions @Movement => new MovementActions(this);
+    public interface IMovementActions
+    {
+        void OnWalk(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+        void OnInteract(InputAction.CallbackContext context);
+    }
+}
---
+++ Scripts/Player/Player_Movement.cs
@@ -0,0 +1,104 @@
+//Copyright (c) FuchsFarbe
+
+using UnityEngine;
+
+/* ============================================
+*               Player Movement
+* --------------------------------------------
+*		Controls the movement of the player
+*	and gets input from the player controller.
+*  ===========================================
+*/
+namespace Tales_Of_Enariel
+{
+	public class Player_Movement : MonoBehaviour
+	{
+		#region Variables
+
+		//References
+		private Player_Controller pc;
+		//Variables
+		//Serialized fields
+		[SerializeField] private float baseSpeed;
+		private float playerWalkSpeed;
+		[SerializeField] private float runningSpeed;
+		[SerializeField] private float playerZed = -2f;
+		[SerializeField] private float turnSmoothVelocity = 0.1f, turnSmoothTime = 0.1f;
+		[SerializeField] private bool isRunning = false;
+
+		private Vector3 direction;
+
+		public float PlayerWalkSpeed 
+		{
+			get
+			{
+				if (pc.RunInput.ReadValue<float>() > 0.1f)
+				{
+					if (playerWalkSpeed == runningSpeed)
+					{
+						return runningSpeed;
+					}
+					else if (playerWalkSpeed < runningSpeed)
+					{
+						return playerWalkSpeed += Time.deltaTime;
+					}
+				}
+				else if (pc.RunInput.ReadValue<float>() < 0.1f)
+				{
+					if (playerWalkSpeed == baseSpeed)
+					{
+						return baseSpeed;
+					}
+					else if (playerWalkSpeed < baseSpeed)
+					{
+						return playerWalkSpeed += Time.deltaTime;
+					}
+				}
+				return playerWalkSpeed = baseSpeed;
+			} 
+		}
+
+		#endregion
+
+		//Get components
+		private void Awake()
+		{
+			pc = GameObject.FindGameObjectWithTag("GameController").GetComponent<Player_Controller>();
+		}
+
+		private void Update()
+		{
+			//Ensures that the players zed axis stays at -2
+			if (pc.PlayerBody.transform.position.z != playerZed)
+			{
+				pc.PlayerBody.transform.position = new Vector3(0f, 0f, playerZed);
+			}
+			//Revert change and direction
+			pc.Change = Vector2.zero;
+			direction = Vector3.zero;
+			//Move player
+			MovePlayer();
+		}
+
+		private void MovePlayer()
+		{
+			//Check if change is changed with input
+			pc.Change = pc.MoveInput.ReadValue<Vector2>();
+			direction = new Vector3(pc.Change.x, 0f, pc.Change.y);
+
+			direction.Normalize();
+
+			if (pc.Change != Vector2.zero)
+			{
+				//Find angles to rotate players
+				float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+				float angle = Mathf.SmoothDampAngle(pc.PlayerBody.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+				//Rotate player in correct direction
+				pc.PlayerBody.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+				//Move player
+				pc.PlayerBody.transform.position = new Vector3(pc.PlayerBody.transform.position.x + pc.Change.x * PlayerWalkSpeed * Time.deltaTime, pc.PlayerBody.transform.position.y + pc.Change.y * PlayerWalkSpeed * Time.deltaTime, playerZed);
+			}
+		}
+	}
+}
